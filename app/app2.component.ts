fdf5b0576060714748d9b240f6594b36276d53b0
Negi <vinay.negi@capgemini.com>
2017-12-27 11:56:46 +0530

    initial commit

--- app/app2.component.ts
+++ app/app2.component.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
 
 @Component({
     selector : 'my-app2',
-    template: '<h3>Hello {{name2}}</h3>'
+    templateUrl: 'app/app2.component.html'
 })
 
-export class App2Component {name2 = 'new Hello World';}
+
+
+export class App2Component {
+    
+pageTitle = 'Product page';
+
+products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://localhost:8808/app/assets/images/garden_cart.jpg"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "http://localhost:8808/app/assets/images/rejoin_Hammer.jpg"
+        }
+    ];
+}
